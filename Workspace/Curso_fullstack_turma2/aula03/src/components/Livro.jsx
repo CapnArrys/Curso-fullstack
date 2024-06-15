5a0a7e6aeef63afaa441d6e6ddaaafad3b752504
CapnArrys <arryshanry@gmail.com>
2024-06-15 14:19:34 -0300

    aula03

---
+++ Workspace/Curso_fullstack_turma2/aula03/src/components/Livro.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Livro = ({ titulo , autor , ano}) => {
+
+
+  return (
+    <div className="livro">
+        <h2>Título: {titulo}</h2>
+        <p>Autor: {autor}</p>
+        <p>Ano: {ano}</p>
+
+    </div>
+  )
+}
+
+export default Livro
